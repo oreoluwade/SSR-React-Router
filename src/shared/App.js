5e63903b02dc3ed269a40052314640ec55cc7ecb
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-05-30 11:24:44 +0100

    render Grid component in app, supplying it props from App.

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Grid from './Grid';
 
 class App extends Component {
   render() {
     return (
       <div>
-        Hello World
+        <Grid data={this.props.data} />
       </div>
     )
   }
